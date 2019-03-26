b1668c805d28eb3859684603aba0b61b5fff6a96
Mikko <32383517+rompsq@users.noreply.github.com>
2019-03-26 14:20:29 +0200

    ajaxiii

---
+++ HTTP-B/precache-manifest.548b825d6e3f051665ce8fafa834f6f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "53c069ef10d9fe90f052",
+    "url": "./static/js/main.bdd75dd0.chunk.js"
+  },
+  {
+    "revision": "f28f6ec550be61c0d2ff",
+    "url": "./static/js/2.c9760b10.chunk.js"
+  },
+  {
+    "revision": "53c069ef10d9fe90f052",
+    "url": "./static/css/main.c37e8a2f.chunk.css"
+  },
+  {
+    "revision": "6554f9a24d640f143f50e6b4c43c71d3",
+    "url": "./index.html"
+  }
+];
